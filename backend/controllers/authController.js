31ca62780aa1c954ec62e351aef148d4d4240d94
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2023-12-27 21:27:49 +0100

    login api

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,26 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+const generateTokens = (id, email, username, name) => {
+  const accessToken = jwt.sign(
+    {
+      UserInfo: {
+        id,
+        email,
+        username,
+        name,
+      },
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "30m" }
+  );
+  const refreshToken = jwt.sign({ email }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "1d",
+  });
+
+  return { accessToken, refreshToken };
+};
+
 const register = async (req, res) => {
   const { name, email, password, picture, username } = req.body;
   if (!name || !email || !password || !date_of_birth)
@@ -28,23 +48,13 @@ const register = async (req, res) => {
       newUser.picture = pictureURL;
     }
 
-    const accessToken = jwt.sign(
-      {
-        userInfo: {
-          id: newUser._id,
-          name: newUser.name,
-          email: newUser.email,
-          username: newUser.username,
-        },
-      },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "30m" }
+    const { accessToken, refreshToken } = generateTokens(
+      newUser._id,
+      newUser.email,
+      newUser.username,
+      newUser.name
     );
-    const refreshToken = jwt.sign(
-      { email: newUser.email },
-      process.env.REFRESH_TOKEN_SECRET,
-      { expiresIn: "1d" }
-    );
+
     newUser.refreshToken = refreshToken;
     await newUser.save();
     res.cookie("access_token", accessToken, {
@@ -66,5 +76,49 @@ const register = async (req, res) => {
     });
   } catch (e) {
     res.status(500).json({ message: e.message });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, pwd } = req.body;
+  if (!email || !pwd)
+    return res
+      .status(400)
+      .json({ message: "Email and password are required." });
+
+  const foundUser = await User.findOne({ email }).exec();
+  if (!foundUser) return res.status(401).json({ message: "User not found" });
+  // evaluate password
+  const match = await bcrypt.compare(pwd, foundUser.password);
+  if (match) {
+    const { accessToken, refreshToken } = generateTokens(
+      foundUser._id,
+      foundUser.email,
+      foundUser.username,
+      foundUser.name
+    );
+    // Saving refreshToken with current user
+    foundUser.refreshToken = refreshToken;
+    await foundUser.save();
+
+    res.cookie("access_token", accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 30 * 60 * 1000, // 59 minutes
+      //domain: "localhost",
+    });
+    res.cookie("refresh_token", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.json({
+      message: "successfully loged in",
+      accessToken,
+    });
+  } else {
+    res.status(401).json({ message: "Invalid password" });
   }
 };
