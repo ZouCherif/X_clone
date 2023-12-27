c30ca6799e7a8331675151f231bae309170e8d61
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2023-12-27 21:46:32 +0100

    google oAuth route

--- backend/routes/auth/login.js
+++ backend/routes/auth/login.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.router();
-const authController = require("./controllers/authController");
+const authController = require("../../controllers/authController");
 
 router.post("/", authController.login);
+router.post("/googleOAuth", authController.handleGoogleAuth);
 
 module.exports = router;
