e3f5bd34b2bd1e9d2b802fc88b3c09327334b91b
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2024-01-19 12:56:17 +0100

    secure firebase config

--- backend/config/FireConnect.js
+++ backend/config/FireConnect.js
@@ -5,13 +5,13 @@ const { getStorage } = require("firebase/storage");
 // var serviceAccount = require("./xclone-927-firebase-adminsdk-14pp3-ee1ef5e83f.json");
 const firebaseConfig = {
   // credential: admin.credential.cert(serviceAccount),
-  apiKey: "AIzaSyDSbBSR4eh8YZYzJ0l34lTLQpdYQcguyNw",
-  authDomain: "xclone-927.firebaseapp.com",
-  projectId: "xclone-927",
-  storageBucket: "xclone-927.appspot.com",
-  messagingSenderId: "119121980940",
-  appId: "1:119121980940:web:9e7da8e1b0335930050829",
-  measurementId: "G-T6PY6V54M8",
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID,
 };
 
 initializeApp(firebaseConfig);
