07f2d181ff09225a6ee31bc00b87c85d3d1beace
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2024-01-24 21:53:10 +0100

    drag and drop profile pic box & delete done

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,3 +1,6 @@
 import logo from "./X_logo.png";
+import person from "./Unknown_person.jpg";
+import camera from "./camera.png";
+import close from "./close.png";
 
-export { logo };
+export { logo, person, camera, close };
--- frontend/src/components/DropZoneBtn.jsx
+++ frontend/src/components/DropZoneBtn.jsx
@@ -1,5 +1,6 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect } from "react";
 import { useDropzone } from "react-dropzone";
+import { person, camera, close } from "../assets";
 function DropZoneBtn({ onFileSelected }) {
   const [file, setFile] = useState(null);
   const onDrop = useCallback(
@@ -9,7 +10,7 @@ function DropZoneBtn({ onFileSelected }) {
     },
     [onFileSelected]
   );
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+  const { getRootProps, getInputProps } = useDropzone({
     onDrop,
     accept: {
       "image/*": [],
@@ -17,23 +18,54 @@ function DropZoneBtn({ onFileSelected }) {
     maxSize: 1024 * 1000,
     maxFiles: 1,
   });
+  const handleFileClose = (event) => {
+    event.stopPropagation();
+    setFile(null);
+  };
+
+  useEffect(() => {
+    if (!file) {
+      onFileSelected(null);
+    }
+  }, [file, onFileSelected]);
 
   return (
-    <div {...getRootProps()}>
+    <div {...getRootProps()} className="w-fit mx-auto mt-20 rounded-full">
       <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
       {file ? (
-        <img
-          src={file}
-          alt="file"
-          className="border border-white w-[200px] h-[200px]"
-        />
+        <div className="relative rounded-full border-2 border-white w-[180px] p-[1px] h-[180px] overflow-hidden flex items-center justify-center">
+          <img
+            src={file}
+            alt="file"
+            className="w-full h-full bg-black opacity-70"
+          />
+          <div className="absolute flex justify-around w-full px-8">
+            <img
+              src={camera}
+              alt="camera"
+              className="w-10 bg-black bg-opacity-50 h-10 rounded-full p-2 hover:cursor-pointer hover:bg-opacity-40"
+            />
+            <img
+              src={close}
+              alt="camera"
+              className="w-10 bg-black bg-opacity-50 h-10 rounded-full p-2 hover:cursor-pointer hover:bg-opacity-40"
+              onClick={handleFileClose}
+            />
+          </div>
+        </div>
       ) : (
-        <div>hello</div>
+        <div className="flex items-center justify-center">
+          <img
+            src={person}
+            alt="person"
+            className="rounded-full border-2 border-white w-[180px] p-[1px] h-[180px] select-none pointer-events-none bg-black opacity-60"
+          />
+          <img
+            src={camera}
+            alt="camera"
+            className="w-10 bg-black bg-opacity-50 absolute h-10 rounded-full p-2 hover:cursor-pointer hover:bg-opacity-25"
+          />
+        </div>
       )}
     </div>
   );
